77887611b753105110b916bf9b668d1adc4fa5e0
George Bockari <bitwhys@gmail.com>
2024-06-05 23:58:45 -0400

    refactor(ui): add tailwind, use shadcn plugin from theme package

---
+++ packages/ui/tailwind.config.ts
@@ -0,0 +1,10 @@
+import { Config } from "tailwindcss";
+import { shadcn } from "@repo/theme/shadcn";
+
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [shadcn],
+} satisfies Config;
