1c922ac0c181bcc5f6cb9a42809cb3927055a88a
George Bockari <bitwhys@gmail.com>
2024-06-05 22:07:46 -0400

    feat(storybook): add storybook package

---
+++ apps/storybook/.storybook/main.ts
@@ -0,0 +1,42 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+import { join, dirname, resolve } from "path";
+
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
+const config: StorybookConfig = {
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    getAbsolutePath("@storybook/addon-onboarding"),
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@chromatic-com/storybook"),
+    getAbsolutePath("@storybook/addon-interactions"),
+  ],
+  framework: {
+    name: getAbsolutePath("@storybook/react-vite"),
+    options: {},
+  },
+  staticDirs: ["../static"],
+  async viteFinal(config, { configType }) {
+    // customize the Vite config here
+    return {
+      ...config,
+      define: { "process.env": {} },
+      resolve: {
+        alias: [
+          {
+            find: "ui",
+            replacement: resolve(__dirname, "../../../packages/ui/"),
+          },
+        ],
+      },
+    };
+  },
+};
+export default config;
